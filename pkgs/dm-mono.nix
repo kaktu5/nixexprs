0bc16510d31f26be22e0ce951faab0a83c549bc3
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-04-17 20:19:22 +0200

    specify version

--- pkgs/dm-mono.nix
+++ pkgs/dm-mono.nix
@@ -3,12 +3,14 @@
   pkgs,
   sources,
 }: let
-  inherit (lib) licenses;
+  inherit (lib) licenses substring;
   inherit (pkgs.stdenv) mkDerivation;
+  inherit (sources) dm-mono;
 in
   mkDerivation {
-    name = "dm-mono";
-    src = sources.dm-mono;
+    pname = "dm-mono";
+    version = substring 0 8 dm-mono.revision;
+    src = dm-mono;
     installPhase = "install -Dm444 $src/exports/*.ttf -t $out/share/fonts/truetype";
     meta = {
       description = "DM Mono is a 3 weight, 3 style family designed for DeepMind";
