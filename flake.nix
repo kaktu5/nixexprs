e92e2f79607230cdc188ea4ed5ff6ced52cbd863
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2025-10-30 16:29:40 +0100

    flake: rewrite, switch to npins

--- flake.nix
+++ flake.nix
@@ -1,40 +1,55 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    lib = import ./lib {inherit (nixpkgs) lib;};
-    mkExprs = pkgs: (import ./exprs {
-      inherit pkgs;
-      sources = import ./npins;
-    });
+  outputs = {self, ...}: let
+    sources = import ./npins;
+    lib = import (sources.nixpkgs + /lib);
+
+    inherit (lib.attrsets) attrValues mapAttrs recursiveUpdate;
+    inherit (lib.lists) foldl';
+
+    forEachSystem = systems: f: (foldl' (acc: system: (f system
+      |> mapAttrs (_: value: {${system} = value;})
+      |> recursiveUpdate acc)) {}
+    systems);
+
+    mkExprs = pkgs: import ./exprs {inherit pkgs sources;};
   in
-    lib.kkts.forEachSystem [
+    forEachSystem [
       "aarch64-darwin"
       "aarch64-linux"
       "x86_64-darwin"
       "x86_64-linux"
     ] (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      exprs = mkExprs pkgs;
-      readme = import ./readme.nix {inherit exprs lib pkgs;};
+      pkgs = let
+        nixpkgs = import sources.nixpkgs {inherit system;};
+        flake-compat = import sources.flake-compat;
+        statix = (flake-compat {src = sources.statix;}).defaultNix.packages.${system}.default;
+      in
+        nixpkgs.extend (_: _: {inherit statix;});
     in {
-      packages = exprs // {inherit readme;};
-      devShells.default = pkgs.mkShellNoCC {nativeBuildInputs = [pkgs.npins];};
+      legacyPackages = mkExprs pkgs;
+
+      devShells.default = pkgs.mkShellNoCC {
+        packages = attrValues {
+          inherit (pkgs) deadnix nil nixd npins statix;
+        };
+      };
+
       formatter = pkgs.writeShellApplication {
-        name = "format";
-        runtimeInputs = with pkgs; [alejandra fd];
+        name = "fmt";
+        runtimeInputs = attrValues {
+          inherit (pkgs) alejandra deadnix fd statix;
+        };
         text = ''
-          fd "$@" -t f -e nix -E npins/ -X alejandra '{}'
+          fd "$@" -t f -e nix -E npins/ -X alejandra --quiet '{}'
+          fd "$@" -t f -e nix -E npins/ -X deadnix --fail '{}'
+          fd "$@" -t f -e nix -E npins/ -x statix check '{}'
         '';
       };
     })
     // {
-      lib = lib.kkts;
       overlays = {
-        default = self.overlays.kkts;
         kkts = _: prev: {kkts = mkExprs prev;};
+        default = self.overlays.kkts;
       };
     };
 }
