998e3a17e793ebd9d11ebc1d75d1e73ed353f4eb
kaktu5 <108426150+kaktu5@users.noreply.github.com>
2026-03-04 15:40:24 +0100

    btop-custom

---
+++ exprs/btop-custom/package.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  lib,
+  sources,
+  autoAddDriverRunpath,
+  cmake,
+  rocmPackages,
+  stdenv,
+  cudaSupport ? config.cudaSupport,
+  rocmSupport ? config.rocmSupport,
+}: let
+  inherit (lib.attrsets) getLib;
+  inherit (lib.licenses) asl20;
+  inherit (lib.lists) optional;
+  inherit (lib.platforms) linux;
+  inherit (lib.strings) optionalString removePrefix;
+  inherit (sources) btop;
+  inherit (stdenv) mkDerivation;
+in
+  mkDerivation {
+    pname = "btop-custom";
+    version = removePrefix "v" btop.version;
+
+    src = btop;
+    patches = [./0001-shorten-process-command-paths.patch];
+
+    nativeBuildInputs = [cmake] ++ optional cudaSupport autoAddDriverRunpath;
+
+    postPhases = optional rocmSupport "postPatchelf";
+    postPatchelf = optionalString rocmSupport ''
+      patchelf --add-rpath ${getLib rocmPackages.rocm-smi}/lib $out/bin/btop
+    '';
+
+    meta = {
+      description = "Custom build of btop";
+      homepage = "https://github.com/aristocratos/btop";
+      license = asl20;
+      platforms = linux;
+      mainProgram = "btop";
+    };
+  }
